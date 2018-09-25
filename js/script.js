4579d39545f4193d1fd56d26ed47950a983a8132
rishabh-malhotra <rishabhmalhotra110796@gmail.com>
2018-09-25 15:58:10 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var app =new Vue({
+    el:'#app',
+    data: {
+        name:'application',
+        images:[
+            { text: 'img/1.jpg', btid:'id1'},
+            { text: 'img/2.jpg' , btid:'id2'},
+            { text: 'img/3.jpg', btid:'id3'},
+            { text: 'img/4.jpg', btid:'id4'},
+            { text: 'img/5.jpg', btid:'id5'},
+            { text: 'img/6.jpg', btid:'id6'}
+        ]
+    }
+})
